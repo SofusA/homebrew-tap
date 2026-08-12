cdab1b1bedf002eaea41f865b1b9d5cc0e27f8e4
Sofus Addington <sofus@addington.dk>
2026-08-12 13:29:13 +0200

    add rssbook

---
+++ Formula/rssbook.rb
@@ -0,0 +1,16 @@
+class Rssbook < Formula
+  desc "Tool for converting rss feeds into epub books"
+  homepage "https://github.com/sofusa/rssbook"
+  license "GPL-3.0"
+  head "https://github.com/sofusa/rssbook.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "text-wallpaper", shell_output("#{bin}/rssbook --help")
+  end
+end
